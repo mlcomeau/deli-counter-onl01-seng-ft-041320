f3c0f05c835205f01b33849d0af521e339abf8e6
Megan Comeau <mlcomeau99@gmail.com>
2020-04-14 20:04:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,9 @@ def line (array)
     puts "The line is currently: #{lineList.join(" ")}"
   end
 end
+
+
+def take_a_number (array, name)
+  array << name 
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+end
