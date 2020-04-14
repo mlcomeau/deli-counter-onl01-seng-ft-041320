56111d463cc5525cfc368535d3a63bc1347a78ee
Megan Comeau <mlcomeau99@gmail.com>
2020-04-14 19:54:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line (array)
   lineList = []
   if array.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
 
   counter = 1
@@ -10,5 +10,5 @@ def line (array)
     counter += 1
   end
 
-  return "The line is currently: #{lineList.join(", ")}"
+  puts "The line is currently: #{lineList.join(", ")}"
 end
