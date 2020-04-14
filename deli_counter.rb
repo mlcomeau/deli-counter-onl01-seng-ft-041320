b6a34f74de6e3b793e229265e4e5e91faba2fcad
Megan Comeau <mlcomeau99@gmail.com>
2020-04-14 19:52:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line (array)
+  lineList = []
+  if array.length == 0
+    return "The line is currently empty."
+  end
+
+  counter = 1
+  array.each do |name|
+    lineList << "#{counter}. #{name}" 
+    counter += 1 
+  end
+
+  return "The line is currently: #{lineList.join(, )}"
+end
+
+     
